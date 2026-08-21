b3302d486eac8cc23e7574c5d2374e2b88deb4af
RicieriF <43136637+RicieriF@users.noreply.github.com>
2026-08-21 04:58:02 -0400

    fix: derive GHOSTCAM IPC names from application id

--- app/src/main/java/io/github/zensu357/camswap/IpcContract.java
+++ app/src/main/java/io/github/zensu357/camswap/IpcContract.java
@@ -3,7 +3,8 @@ package io.github.zensu357.camswap;
 import android.net.Uri;
 
 public final class IpcContract {
-    public static final String AUTHORITY = "io.github.zensu357.camswap.provider";
+    public static final String APP_ID = BuildConfig.APPLICATION_ID;
+    public static final String AUTHORITY = APP_ID + ".provider";
     public static final Uri CONTENT_URI = Uri.parse("content://" + AUTHORITY);
 
     public static final String PATH_CONFIG = "config";
@@ -14,11 +15,11 @@ public final class IpcContract {
     public static final Uri URI_VIDEO = Uri.withAppendedPath(CONTENT_URI, PATH_VIDEO);
     public static final Uri URI_AUDIO = Uri.withAppendedPath(CONTENT_URI, PATH_AUDIO);
 
-    public static final String ACTION_UPDATE_CONFIG = "io.github.zensu357.camswap.ACTION_UPDATE_CONFIG";
-    public static final String ACTION_REQUEST_CONFIG = "io.github.zensu357.camswap.ACTION_REQUEST_CONFIG";
-    public static final String ACTION_NEXT = "io.github.zensu357.camswap.ACTION_CAMSWAP_NEXT";
-    public static final String ACTION_ROTATE = "io.github.zensu357.camswap.ACTION_CAMSWAP_ROTATE";
-    public static final String ACTION_EXIT = "io.github.zensu357.camswap.ACTION_CAMSWAP_EXIT";
+    public static final String ACTION_UPDATE_CONFIG = APP_ID + ".ACTION_UPDATE_CONFIG";
+    public static final String ACTION_REQUEST_CONFIG = APP_ID + ".ACTION_REQUEST_CONFIG";
+    public static final String ACTION_NEXT = APP_ID + ".ACTION_GHOSTCAM_NEXT";
+    public static final String ACTION_ROTATE = APP_ID + ".ACTION_GHOSTCAM_ROTATE";
+    public static final String ACTION_EXIT = APP_ID + ".ACTION_GHOSTCAM_EXIT";
 
     public static final String EXTRA_CONFIG_JSON = "config_json";
     public static final String EXTRA_REQUESTER_PACKAGE = "requester_package";
